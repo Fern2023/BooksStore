47d9028135946cdccbdb6dea72db36b55199ba42
Fern2023 <fernando.guarnizo@gfn.education>
2024-03-15 21:18:15 +0100

    beginn

--- client/vite.config.js
+++ client/vite.config.js
@@ -2,13 +2,13 @@ import { defineConfig } from 'vite'
 import react from '@vitejs/plugin-react'
 
 // https://vitejs.dev/config/
-// export default defineConfig({
-//   plugins: [react()],
-// })
-
-
 export default defineConfig({
-  build: {
-    outDir: 'dist'
-  }
-});
+  plugins: [react()],
+})
+
+
+// export default defineConfig({
+//   build: {
+//     outDir: 'dist'
+//   }
+// });
